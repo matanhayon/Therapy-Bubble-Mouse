0923f362a0412553bf0f5ee692af08fa5fcb8422
matanhayon <121755360+matanhayon@users.noreply.github.com>
2024-05-23 21:22:17 +0300

    Update Secured_Gyro_Clicker.ino - added blink to test hardware

--- Secured_Gyro_Clicker/Secured_Gyro_Clicker.ino
+++ Secured_Gyro_Clicker/Secured_Gyro_Clicker.ino
@@ -9,6 +9,12 @@ const int button_pin = 2;
 const int security_pin = 12;
 int previous_button_state = 0;
 
+unsigned long previousTime = 0;
+unsigned long currentTime;
+const unsigned long interval = 500;
+bool isHigh = true;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -27,22 +33,34 @@ void loop()
 {
   if(digitalRead(security_pin)==LOW)
   {
-    // put your main code here, to run repeatedly:
+    // Accelometer:
   	mpu.getMotion6(&ax, &ay, &az, &gx, &gz, &gy);
 	  vx = -(gx+260)/150;
 	  vy = (gz+100)/150;
+    Mouse.move(vx, 0);
+    delay(20);
+
+    //click
 		int current_button_state = digitalRead(button_pin);
     if (current_button_state == LOW)
     {
       Mouse.click();
-     // Serial.println("clicked");
+      Serial.println("clicked");
     }
     else
     {
- //     Serial.println("else - current button state high " + current_button_state);
+      Serial.println("else - current button state high " + current_button_state);
     }
 
-    Mouse.move(vx, 0);
-    delay(20);
+    //Blink
+    currentTime = millis();
+    if(currentTime - previousTime >= interval)
+    {
+      digitalWrite(13, isHigh ? HIGH : LOW);
+      isHigh = !isHigh;
+      previousTime = currentTime;
+    }
+
+
   }  
 }
